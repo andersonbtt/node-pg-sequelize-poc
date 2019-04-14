c1e84c8930bb496facd175ea1aa435f917605d09
Anderson Bittencourt <anderson_btt@hotmail.com>
2019-04-14 13:51:36 -0300

    set up first database framework

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("Hello World!");
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('dev','dev','dev', {
+	host: 'localhost',
+	dialect: 'postgres',
+	pool: {
+		max: 9,
+		min: 0,
+		idle: 10000
+	}
+});
+
+sequelize.authenticate().then(() => {
+	console.log("Success!");
+
+	var Posts = sequelize.define('posts', {
+		title: {
+			type: Sequelize.STRING
+		},
+		content: {
+			type: Sequelize.STRING
+		},
+	}, {
+		freezeTableName: true
+	});
+
+	Posts.sync({force: true}).then(function(){
+		return Posts.create({
+			title: 'Getting started with PostgreSQL and Sequelize',
+			content: 'Hello there'
+		});
+	});	
+
+}).catch((err) => {
+	console.log(err);
+});
